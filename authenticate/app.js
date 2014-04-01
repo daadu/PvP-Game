8e308d2ed7164a99b0448881cde323c06345e6cb
EXCELSIOR <harsh.bhikadiya@gmail.com>
2014-04-02 01:14:40 +0530

    code cleaning

--- authenticate/app.js
+++ authenticate/app.js
@@ -85,6 +85,6 @@ app.get('/js/game.js',function(req,res){
 	})
 })
 
-app.listen(8080);
+app.listen(process.env.PORT||8080);
 console.log(app.address())
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
